345faa9c02c26b59705dd27b509311c0be7c0471
ChanheeKim <cgim971@gmail.com>
2022-12-09 13:00:01 +0900

    로비 씬 추가

---
+++ apiframeworktest/Scene_Lobby.cpp
@@ -0,0 +1,22 @@
+#include "pch.h"
+#include "Scene_Lobby.h"
+
+Scene_Lobby::Scene_Lobby()
+{
+}
+
+Scene_Lobby::~Scene_Lobby()
+{
+}
+
+void Scene_Lobby::Enter()
+{
+}
+
+void Scene_Lobby::Exit()
+{
+}
+
+void Scene_Lobby::Update()
+{
+}
---
+++ apiframeworktest/Scene_Lobby.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "Scene.h"
+class Scene_Lobby :
+    public Scene
+{
+private:
+public:
+    Scene_Lobby();
+    virtual ~Scene_Lobby();
+public:
+    virtual void Enter() override;
+    virtual void Exit() override;
+    virtual void Update() override;
+};
+
