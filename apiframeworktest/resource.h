e74bcbbcda42c37f7d789015f47871e27a6e7e19
ChanheeKim <cgim971@gmail.com>
2022-12-20 19:35:31 +0900

    1

--- apiframeworktest/resource.h
+++ apiframeworktest/resource.h
@@ -1,6 +1,6 @@
 ﻿//{{NO_DEPENDENCIES}}
-// Microsoft Visual C++ generated include file.
-// Used by apiframeworktest.rc
+// Microsoft Visual C++에서 생성한 포함 파일입니다.
+// apiframeworktest.rc에서 사용되고 있습니다.
 //
 #define IDC_MYICON                      2
 #define IDD_APIFRAMEWORKTEST_DIALOG     102
@@ -12,7 +12,7 @@
 #define IDI_SMALL                       108
 #define IDC_APIFRAMEWORKTEST            109
 #define IDR_MAINFRAME                   128
-#define IDR_ACCELERATOR1                129
+#define IDC_BUTTON1                     1000
 #define IDC_STATIC                      -1
 
 // Next default values for new objects
@@ -20,9 +20,9 @@
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NO_MFC                     1
-#define _APS_NEXT_RESOURCE_VALUE        132
-#define _APS_NEXT_COMMAND_VALUE         32772
-#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_RESOURCE_VALUE        129
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           110
 #endif
 #endif
